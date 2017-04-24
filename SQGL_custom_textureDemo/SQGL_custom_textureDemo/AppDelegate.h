026a43ef3009fb87377bf5396d20e279c5fde14c
qbshen <qbshen_whaley>
2017-04-24 20:41:34 +0800

    create pro

---
+++ SQGL_custom_textureDemo/SQGL_custom_textureDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQGL_custom_textureDemo
+//
+//  Created by qbshen on 2017/4/24.
+//  Copyright © 2017年 qbshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
